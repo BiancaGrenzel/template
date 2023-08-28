64eca73b649d2140930de839cf29c7c9da5c95b3
Bianca <biancagrenzel@gmail.com>
2023-08-28 17:15:20 -0300

    feat: new babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = { 
   presets: [
+    // O @babel/preset-env converte código JavaScript moderno, que 
+    // pode conter recursos não suportados por todos os navegadores, 
+    // em uma versão que funcione na maioria dos navegadores.
     '@babel/preset-env',
+    // Define o preset do babel para react, para poder
+    // converter arquivos .jsx
     '@babel/preset-react'
   ]  
 }
